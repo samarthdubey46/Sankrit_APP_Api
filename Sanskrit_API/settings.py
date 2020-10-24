1c792dddfa2d12fcff913be5c9ddf147f1f86e00
Samarth <samarthdubey46@gmail.com>
2020-10-25 02:13:33 +0530

    Added

--- Sanskrit_API/settings.py
+++ Sanskrit_API/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -153,3 +155,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
+django_heroku.settings(locals())
