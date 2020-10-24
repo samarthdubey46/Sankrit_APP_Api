a632e48e7201f5b1b71b92c5ffdbe92ea1552b2b
Samarth <samarthdubey46@gmail.com>
2020-10-25 02:00:02 +0530

    Added

--- Sanskrit_API/settings.py
+++ Sanskrit_API/settings.py
@@ -153,6 +153,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
