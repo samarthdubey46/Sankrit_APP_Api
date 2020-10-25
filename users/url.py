181571de4acad087a937abd06022b88a8dd104f0
Samarth <samarthdubey46@gmail.com>
2020-10-25 20:45:55 +0530

    Changed Structure

--- users/url.py
+++ users/url.py
@@ -1,29 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
-from .views import (
-    registration_view,
-    ObtainAuthTokenView,
-    account_properties_view,
-    update_account_view,
-    does_account_exist_view,
-    ChangePasswordView,
-    FriendsView,
-    UserViewSet
-)
-from rest_framework.authtoken.views import obtain_auth_token
-
+from rest_framework.authtoken.views import obtain_auth_token  # <-- Here
+from .views import *
 app_name = 'users'
 router = DefaultRouter()
-router.register('delete',UserViewSet)
+router.register('all', AllinOne)
 urlpatterns = [
-    path('check_if_account_exists/', does_account_exist_view, name="check_if_account_exists"),
-    path('change_password/', ChangePasswordView.as_view(), name="change_password"),
-    path('properties', account_properties_view, name="properties"),
-    path('properties/update', update_account_view, name="update"),
-    path('login', ObtainAuthTokenView.as_view(), name="login"),
-    path('register', registration_view, name="register"),
-    path('list', FriendsView.as_view()),
-    path('',include(router.urls))
-
+    path('leaderboard/',LeaderBoard.as_view()),
+    path('login/', login, name='api_token_auth'),
+    path('', include(router.urls)),
+    # <-- And here
 ]
